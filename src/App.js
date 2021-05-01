01a9f967c2ea6b71508a6c96768e91e305a45da4
Mikołaj Cięszczyk <mikolaj.cieszczyk@gmail.com>
2021-05-01 14:44:21 +0200

    scrolling nav done but without passing props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
-import React, { useState, useEffect } from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import React, { useEffect, useState, useRef, Component } from "react";
+import { useWindowScroll } from "react-use";
 
-import Navigation from "components/Navigation/Navigation";
 import AboutUs from "components/AboutUs/AboutUs";
 import PackagesBar from "components/Packages/PackagesBar";
 import SeaStandardPackage from "components/Packages/SeaStandardPackage";
@@ -16,60 +10,211 @@ import OneOffServicesPackage from "components/Packages/OneOffServicesPackage";
 import CommercialAndPublic from "components/CommercialAndPublic";
 import DefaultForm from "components/Form/DefaultForm";
 import Footer from "components/Footer/Footer";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBars,
+  faSortDown,
+  faSortUp,
+  faTimes,
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  faFacebook,
+  faInstagram,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
 
-import { NavHashLink } from 'react-router-hash-link';
+const ScrollIndicator = () => {
+  const { x, y } = useWindowScroll();
+  const [scrolled, setScrolled] = useState(0);
+
+  useEffect(() => {
+    const height =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+    setScrolled((y / height) * 100);
+  }, [y]);
+};
 
 export default function App() {
-  
-  
-  
+  const [showNav, setShowNav] = useState(true);
+  const [showPackagesNav, setShowPackagesNav] = useState(false);
+  const [showEcoPackagesNav, setShowEcoPackagesNav] = useState(false);
+
+  const aboutUsSection = useRef(null);
+  const standardPackageSection = useRef(null);
+  const premiumPackageSection = useRef(null);
+  const ecoPackageSection = useRef(null);
+  const oneOffSection = useRef(null);
+  const commercialSection = useRef(null);
+  const contactSection = useRef(null);
+
+  const goToAboutUsSection = () =>
+    window.scrollTo({
+      top: aboutUsSection.current.offsetTop,
+      behavior: "smooth",
+    });
+
+  const goToStandardPackageSection = () =>
+    window.scrollTo({
+      top: standardPackageSection.current.offsetTop,
+      behavior: "smooth",
+    });
+
+  const goToPremiumPackageSection = () =>
+    window.scrollTo({
+      top: premiumPackageSection.current.offsetTop,
+      behavior: "smooth",
+    });
+
+  const goToEcoPackageSection = () =>
+    window.scrollTo({
+      top: ecoPackageSection.current.offsetTop,
+      behavior: "smooth",
+    });
+
+  const goToOneOffSection = () =>
+    window.scrollTo({
+      top: oneOffSection.current.offsetTop,
+      behavior: "smooth",
+    });
+
+  const goToCommercialSection = () =>
+    window.scrollTo({
+      top: commercialSection.current.offsetTop,
+      behavior: "smooth",
+    });
+
+  const goToContactSection = () =>
+    window.scrollTo({
+      top: contactSection.current.offsetTop,
+      behavior: "smooth",
+    });
+
   return (
     <div className="App">
-    <Router>
-    {/* <div>
-          <li>
-            <NavHashLink to="/aboutUsId">about us</NavHashLink>
-          </li>
-          <li>
-            <NavHashLink to="/standardpackage">standard package</NavHashLink>
-          </li>
-          <li>
-            <NavHashLink to="/premiumpackage">premium package</NavHashLink>
-          </li>
-    </div> */}
-      <Navigation />
+      <header>
+        <div
+          className="open-nav position-fixed ml-3 pt-4"
+          onClick={() => setShowNav(!showNav)}
+        >
+          <FontAwesomeIcon icon={faBars} className="fa-3x" />
+          <span className="nav__content-hamburger-signature">menu</span>
+        </div>
+        {showNav && (
+          <nav className="d-flex flex-column justify-content-between">
+            <div className="container w-75 d-flex flex-column align-items-start mt-4">
+              <div
+                className="close-nav mb-3"
+                onClick={() => setShowNav(!showNav)}
+              >
+                x
+              </div>
+              <div className="mt-5 mb-2" onClick={goToAboutUsSection}>
+                about us
+              </div>
+              <div className="my-2">
+                <span onClick={() => setShowPackagesNav(!showPackagesNav)}>
+                  packages{" "}
+                  <FontAwesomeIcon
+                    icon={showPackagesNav ? faSortDown : faSortUp}
+                    className="ml-3"
+                  />
+                </span>
+                {showPackagesNav && (
+                  <ul className="container mb-0 font-weight-normal">
+                    <li className="my-2" onClick={goToStandardPackageSection}>
+                      standard
+                    </li>
+                    <li className="my-2" onClick={goToPremiumPackageSection}>
+                      premium
+                    </li>
+                    <li className="mt-2">
+                      <span
+                        onClick={() =>
+                          setShowEcoPackagesNav(!showEcoPackagesNav)
+                        }
+                      >
+                        eco{" "}
+                        <FontAwesomeIcon
+                          icon={showEcoPackagesNav ? faSortDown : faSortUp}
+                          className="ml-3"
+                        />
+                      </span>
+                    </li>
+                    {showEcoPackagesNav && (
+                      <ul className="container">
+                        <li className="my-2" onClick={goToEcoPackageSection}>
+                          basic
+                        </li>
+                        <li className="my-2" onClick={goToEcoPackageSection}>
+                          medium
+                        </li>
+                        <li className="mt-2" onClick={goToEcoPackageSection}>
+                          advanced
+                        </li>
+                      </ul>
+                    )}
+                  </ul>
+                )}
+              </div>
+              <div className="my-2" onClick={goToOneOffSection}>
+                one-off
+              </div>
+              <div className="my-2" onClick={goToCommercialSection}>
+                commercial
+              </div>
+              <div className="my-2" onClick={goToContactSection}>
+                contact
+              </div>
+            </div>
+            <div className="container w-75 mb-5 nav__footer">
+              <div className="nav-footer-branding mb-3" />
+              <div className="nav__footer--social">
+                <a
+                  href="https://instagram.com/northhillgardens/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-white mr-2"
+                >
+                  <FontAwesomeIcon icon={faInstagram} className="fa mx-1" />
+                </a>
+                <a
+                  href="https://twitter.com/northhilldesign/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-white mr-2"
+                >
+                  <FontAwesomeIcon icon={faTwitter} className="fa mx-1" />
+                </a>
+                <a
+                  href="https://facebook.com/northhillgardens/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-white"
+                >
+                  <FontAwesomeIcon icon={faFacebook} className="fa mx-1" />
+                </a>
+              </div>
+            </div>
+          </nav>
+        )}
+      </header>
+      <span ref={aboutUsSection}></span>
       <AboutUs />
       <PackagesBar />
+      <span ref={standardPackageSection}></span>
       <SeaStandardPackage />
+      <span ref={premiumPackageSection}></span>
       <SkyPremiumPackage />
+      <span ref={ecoPackageSection}></span>
       <EarthEcoPackage />
+      <span ref={oneOffSection}></span>
       <OneOffServicesPackage />
+      <span ref={commercialSection}></span>
       <CommercialAndPublic />
+      <span ref={contactSection}></span>
       <DefaultForm />
       <Footer />
-
-        <Switch>
-          <Route path="/aboutus">
-            <AboutUs />
-          </Route>
-          <Route path="/standardpackage">
-            <SeaStandardPackage />
-          </Route>
-          <Route path="/premiumpackage">
-            <SkyPremiumPackage />
-          </Route>
-          <Route path="/ecopackage">
-            <EarthEcoPackage />
-          </Route>
-          <Route path="/oneoffservices">
-            <OneOffServicesPackage />
-          </Route>
-          <Route path="/commercialpublic">
-            <CommercialAndPublic />
-          </Route>
-        </Switch>
-      </Router>
     </div>
   );
 }
-
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,145 +1,145 @@
-import React, { Component } from "react";
-
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTimes, faBars, faSortDown, faSortUp } from "@fortawesome/free-solid-svg-icons";
-
-import {
-  faInstagram,
-  faTwitter,
-  faFacebook,
-} from "@fortawesome/free-brands-svg-icons";
-
-
-export default class Navigation extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showMenu: false,
-      collapseFirstLevel: false,
-      collapseSecondLevel: false,
-    };
-
-    this.toggleMenu = this.toggleMenu.bind(this);
-    this.toggleCollapseFirstLevel = this.toggleCollapseFirstLevel.bind(this);
-    this.toggleCollapseSecondLevel = this.toggleCollapseSecondLevel.bind(this);
-  }
-
-  toggleMenu(e) {
-    e.preventDefault();
-    this.setState({ showMenu: !this.state.showMenu });
-  }
-
-  toggleCollapseFirstLevel() {
-    this.setState({ collapseFirstLevel: !this.state.collapseFirstLevel });
-  }
-
-  toggleCollapseSecondLevel() {
-    this.setState({ collapseSecondLevel: !this.state.collapseSecondLevel });
-  }
-
-  render() {
-    return (
-      <header>
-        <nav className={this.state.showMenu ? "sidebar active" : "sidebar"}>
-          <div className="hide-sidebar-btn">
-            <div onClick={this.toggleMenu} className="dismiss">
-              <FontAwesomeIcon icon={faTimes} />
-            </div>
-          </div>
-
-          <ul className="list-unstyled my-0 py-0">
-            <li>
-              <a id="aboutUsBtn" href="#">about us</a>
-            </li>
-            <li>
-              <span onClick={this.toggleCollapseFirstLevel}>
-                packages
-                <FontAwesomeIcon
-                  icon={
-                    this.state.collapseFirstLevel
-                      ? faSortDown
-                      : faSortUp
-                    }
-                  className="ml-2"
-                />
-              </span>
-              <ul
-                className={
-                  this.state.collapseFirstLevel
-                    ? "nav-collapse__first-level"
-                    : "collapse-inactive"
-                }
-              >
-                <li className="ml-5">standard</li>
-                <li className="ml-5">premium</li>
-                <li>
-                  <span
-                    className="ml-5 p-0 d-inline"
-                    onClick={this.toggleCollapseSecondLevel}
-                  >
-                    eco
-                    <FontAwesomeIcon
-                      icon={
-                        this.state.collapseSecondLevel
-                          ? faSortDown
-                          : faSortUp
-                      }
-                      className="ml-3"
-                    />
-                  </span>
-                  <ul
-                    className={
-                      this.state.collapseSecondLevel
-                        ? "nav-collapse__second-level"
-                        : "collapse-inactive"
-                    }
-                  >
-                    <li className="ml-5 pl-4">basic</li>
-                    <li className="ml-5 pl-4">medium</li>
-                    <li className="ml-5 pl-4">advanced</li>
-                  </ul>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a href="#one-off">
-                <i className="fas fa-user"></i>one-off
-              </a>
-            </li>
-            <li>
-              <a href="#commercial">
-                <i className="fas fa-pencil-alt"></i>commercial
-              </a>
-            </li>
-            <li>
-              <a href="#contact">
-                <i className="fas fa-envelope"></i>contact
-              </a>
-            </li>
-          </ul>
-
-          <div className="nav__footer ml-2 d-flex flex-column">
-            <div className="nav-footer-branding" />
-
-            <div className="nav__footer--social mt-4">
-              <FontAwesomeIcon icon={faInstagram} className="fa mx-1" />
-              <FontAwesomeIcon icon={faTwitter} className="fa mx-1" />
-              <FontAwesomeIcon icon={faFacebook} className="fa mx-1" />
-            </div>
-          </div>
-        </nav>
-
-        <div className="content">
-          <a
-            onClick={this.toggleMenu}
-            className="btn-customized open-menu"
-            href="#"
-          >
-            <FontAwesomeIcon icon={faBars} className="fa-3x" />
-            <span className="nav__content-hamburger-signature">menu</span>
-          </a>
-        </div>
-      </header>
-    );
-  }
-}
+// import React, { Component } from "react";
+//
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faTimes, faBars, faSortDown, faSortUp } from "@fortawesome/free-solid-svg-icons";
+//
+// import {
+//   faInstagram,
+//   faTwitter,
+//   faFacebook,
+// } from "@fortawesome/free-brands-svg-icons";
+//
+//
+// export default class Navigation extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       showMenu: false,
+//       collapseFirstLevel: false,
+//       collapseSecondLevel: false,
+//     };
+//
+//     this.toggleMenu = this.toggleMenu.bind(this);
+//     this.toggleCollapseFirstLevel = this.toggleCollapseFirstLevel.bind(this);
+//     this.toggleCollapseSecondLevel = this.toggleCollapseSecondLevel.bind(this);
+//   }
+//
+//   toggleMenu(e) {
+//     e.preventDefault();
+//     this.setState({ showMenu: !this.state.showMenu });
+//   }
+//
+//   toggleCollapseFirstLevel() {
+//     this.setState({ collapseFirstLevel: !this.state.collapseFirstLevel });
+//   }
+//
+//   toggleCollapseSecondLevel() {
+//     this.setState({ collapseSecondLevel: !this.state.collapseSecondLevel });
+//   }
+//
+//   render() {
+//     return (
+//       <header>
+//         <nav className={this.state.showMenu ? "sidebar active" : "sidebar"}>
+//           <div className="hide-sidebar-btn">
+//             <div onClick={this.toggleMenu} className="dismiss">
+//               <FontAwesomeIcon icon={faTimes} />
+//             </div>
+//           </div>
+//
+//           <ul className="list-unstyled my-0 py-0">
+//             <li>
+//               <a id="aboutUsBtn" href="#">about us</a>
+//             </li>
+//             <li>
+//               <span onClick={this.toggleCollapseFirstLevel}>
+//                 packages
+//                 <FontAwesomeIcon
+//                   icon={
+//                     this.state.collapseFirstLevel
+//                       ? faSortDown
+//                       : faSortUp
+//                     }
+//                   className="ml-2"
+//                 />
+//               </span>
+//               <ul
+//                 className={
+//                   this.state.collapseFirstLevel
+//                     ? "nav-collapse__first-level"
+//                     : "collapse-inactive"
+//                 }
+//               >
+//                 <li className="ml-5">standard</li>
+//                 <li className="ml-5">premium</li>
+//                 <li>
+//                   <span
+//                     className="ml-5 p-0 d-inline"
+//                     onClick={this.toggleCollapseSecondLevel}
+//                   >
+//                     eco
+//                     <FontAwesomeIcon
+//                       icon={
+//                         this.state.collapseSecondLevel
+//                           ? faSortDown
+//                           : faSortUp
+//                       }
+//                       className="ml-3"
+//                     />
+//                   </span>
+//                   <ul
+//                     className={
+//                       this.state.collapseSecondLevel
+//                         ? "nav-collapse__second-level"
+//                         : "collapse-inactive"
+//                     }
+//                   >
+//                     <li className="ml-5 pl-4">basic</li>
+//                     <li className="ml-5 pl-4">medium</li>
+//                     <li className="ml-5 pl-4">advanced</li>
+//                   </ul>
+//                 </li>
+//               </ul>
+//             </li>
+//             <li>
+//               <a href="#one-off">
+//                 <i className="fas fa-user"></i>one-off
+//               </a>
+//             </li>
+//             <li>
+//               <a href="#commercial">
+//                 <i className="fas fa-pencil-alt"></i>commercial
+//               </a>
+//             </li>
+//             <li>
+//               <a href="#contact">
+//                 <i className="fas fa-envelope"></i>contact
+//               </a>
+//             </li>
+//           </ul>
+//
+//           <div className="nav__footer ml-2 d-flex flex-column">
+//             <div className="nav-footer-branding" />
+//
+//             <div className="nav__footer--social mt-4">
+//               <FontAwesomeIcon icon={faInstagram} className="fa mx-1" />
+//               <FontAwesomeIcon icon={faTwitter} className="fa mx-1" />
+//               <FontAwesomeIcon icon={faFacebook} className="fa mx-1" />
+//             </div>
+//           </div>
+//         </nav>
+//
+//         <div className="content">
+//           <a
+//             onClick={this.toggleMenu}
+//             className="btn-customized open-menu"
+//             href="#"
+//           >
+//             <FontAwesomeIcon icon={faBars} className="fa-3x" />
+//             <span className="nav__content-hamburger-signature">menu</span>
+//           </a>
+//         </div>
+//       </header>
+//     );
+//   }
+// }
